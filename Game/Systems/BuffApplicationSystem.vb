27aab7b27c13f9d7df2e11fb5deb364cdff65eb9
Azevedo12322 <franciscodrazevedo@gmail.com>
2026-01-22 17:54:36 +0000

    Added fast exits

--- Game/Systems/BuffApplicationSystem.vb
+++ Game/Systems/BuffApplicationSystem.vb
@@ -4,41 +4,53 @@
     Public Sub Update(world As World, dt As Single) Implements ISystem.Update
         For Each ev In world.CollisionEvents
 
-            Dim playerID As Integer = -1
-            Dim buffID As Integer = -1
+            Dim aIsBuff = world.Buffs.HasComponent(ev.entityA)
+            Dim bIsBuff = world.Buffs.HasComponent(ev.entityB)
 
-            If world.Players.HasComponent(ev.entityA) AndAlso world.Buffs.HasComponent(ev.entityB) Then
-                playerID = ev.entityA
-                buffID = ev.entityB
-            ElseIf world.Buffs.HasComponent(ev.entityA) AndAlso world.Players.HasComponent(ev.entityB) Then
+            If Not aIsBuff AndAlso Not bIsBuff Then
+                Continue For
+            End If
+
+            Dim buffID As Integer
+            Dim candidatePlayerID As Integer
+
+            If aIsBuff Then
                 buffID = ev.entityA
-                playerID = ev.entityB
+                candidatePlayerID = ev.entityB
+            Else
+                buffID = ev.entityB
+                candidatePlayerID = ev.entityA
             End If
 
-            If playerID <> -1 AndAlso buffID <> -1 Then
-                ApplyBuff(world, playerID, buffID)
+            If Not world.Players.HasComponent(candidatePlayerID) Then
+                Continue For
             End If
+
+            Dim buff = world.Buffs.GetComponent(buffID)
+            If buff.isConsumed Then
+                Continue For
+            End If
+
+            ApplyBuff(world, candidatePlayerID, buff, buffID)
         Next
     End Sub
 
-    Private Sub ApplyBuff(world As World, playerID As Integer, buffID As Integer)
-        Dim buff = world.Buffs.GetComponent(buffID)
-
-        If buff.isConsumed Then Return
+    Private Sub ApplyBuff(world As World, playerID As Integer, buff As BuffComponent, buffID As Integer)
 
         Select Case buff.type
             Case BuffType.HealthRegen
                 Dim healthBuff = TryCast(buff, HealthBuff)
 
-                If healthBuff IsNot Nothing AndAlso world.Healths.HasComponent(playerID) Then
-                    Dim hp = world.Healths.GetComponent(playerID)
-                    hp.health += healthBuff.healthRegen
+                If healthBuff Is Nothing OrElse Not world.Healths.HasComponent(playerID) Then
+                    Return
+                End If
 
-                    If hp.health > hp.maxHealth Then
-                        hp.health = hp.maxHealth
-                    End If
+                Dim hp = world.Healths.GetComponent(playerID)
 
-                    Debug.WriteLine("Curado! Vida: " & hp.health & "/" & hp.maxHealth)
+                hp.health += healthBuff.healthRegen
+
+                If hp.health > hp.maxHealth Then
+                    hp.health = hp.maxHealth
                 End If
         End Select
 
